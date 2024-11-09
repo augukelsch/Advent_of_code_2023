ab2ba38e3c3deb3b9908ed93ce3a82b0deb55aaa
Augusto Kelsch <91554980+augukelsch@users.noreply.github.com>
2024-11-08 21:57:29 -0300

    Challenge solved

--- 4-1.js
+++ 4-1.js
@@ -2,6 +2,30 @@ const fs = require("fs");
 
 const out = fs.readFileSync("4-input.txt").toString().split("\r\n")
 
+var output = [];
 
+for(let i = 0 ; i < out.length; i++){ //
+    const winners = out[i].slice(10,39).replaceAll("  "," ").split(" ");
+    const list = out[i].slice(42,117).replaceAll("  "," ").split(" ");
 
-console.log(out)
+    var cardWinners = 0;
+    for (let j = 0; j < winners.length; j++) {
+        if(winners[j] == ''){
+            winners[j] == 'ZZ'
+        }else if(list.indexOf(winners[j]) > -1){
+            cardWinners++;
+        }
+    }
+
+    var points = Math.pow(2,cardWinners-1);
+
+    if(cardWinners == 0){
+        points = 0;
+    }
+    if(cardWinners == 1){
+        points = 1;
+    }
+    output.push(points);
+}
+const result = output.reduce((total,a) => total+=a,0)
+console.log(result)
